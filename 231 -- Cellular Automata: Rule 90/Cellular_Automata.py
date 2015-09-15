7f9a3a4825148beefa8bcb5b0dd8e5019a18da28
fugitiveinkc <blacksg1909@gmail.com>
2015-09-15 01:14:50 -0500

    Updated 231 notes

--- 231 -- Cellular Automata: Rule 90/Cellular_Automata.py
+++ 231 -- Cellular Automata: Rule 90/Cellular_Automata.py
@@ -4,4 +4,13 @@ Title: Cellular Automata: Rule 90
 
 Objective: Using Rule 90 (XOR left and right neighbors) in one dimension, print out next 25 steps.
 
+Process:
+	1) Read input
+	2) Create row based on input
+	3) Calculate new row input
+	4) LOOP
 ''' 
+	
+	
+
+
