5689748e5482a02b1eb376d5dcc41fcf0f136baa
fugitiveinkc <blacksg1909@gmail.com>
2015-09-19 20:21:01 -0500

    Starting 232 Intermediate

---
+++ 232 -- Where Sould Grandma's House Go?/minimum_distance.py
@@ -0,0 +1,14 @@
+'''
+
+Title: Where Should Grandma's House Go?
+
+Objective: Given a set of N cartesian coordinates, find the two points with the least distance between them.
+
+Current thoughts:
+	-Avoid saving the distances into memory.
+	-Perhaps it's easier just to save the current distance and change it based on if another distance exceeds it.
+
+'''
+
+#Function that finds minimum distance between two points and tells you the distance and the points. Feel like recursion?
+		
