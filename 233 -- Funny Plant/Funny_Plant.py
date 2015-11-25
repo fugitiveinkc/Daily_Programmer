4106b0b3ff8327f23eb540f28b431122bd3e0e5d
fugitiveinkc <blacksg1909@gmail.com>
2015-11-25 00:44:25 -0600

    Getting started on 233 [easy]

---
+++ 233 -- Funny Plant/Funny_Plant.py
@@ -0,0 +1,17 @@
+'''
+
+Title: 233 -- Funny Plant
+
+Goal: You have a plant who's fruit can feed 1 person for a whole week.  Fruits are only viable after 1 week.  As the weeks increase, the fruits produce 1 more fruit than the week before.  You are give a number of people and a number of fruit to start with.  Calculate the number of weeks you'll need to produce enough fruit for everyone.
+
+
+'''
+
+#--Solution 1 : 11/24/2015--#
+
+#Read input from command line
+
+
+
+
+
