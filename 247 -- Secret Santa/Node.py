4d5fd2d19a48ed791d1e972d31baa7edd0af449f
fugitiveinkc <blacksg1909@gmail.com>
2015-12-29 17:13:18 -0600

    Almost solved.  Still links family members.

---
+++ 247 -- Secret Santa/Node.py
@@ -0,0 +1,4 @@
+class node:
+	def __init__(self, name, family):
+		self.name = name
+		self.family = family
--- 247 -- Secret Santa/solution_1.py
+++ 247 -- Secret Santa/solution_1.py
@@ -6,22 +6,54 @@ Objective: Give a list of names and families, construct a secret santa map witho
 
 '''
 
+
 #Import necessary libraries
 
+from Node import node
 import random
 random.seed()
 
+
 #Function to traverse names
 
+def traversal(a_node): #Error: Has the potential to link families
+	unvisited.remove(a_node)
+	if not unvisited:
+		return a_node.name
+	else:
+		#1) select name from unvisited
+		#2) If name is in family, reselect
+		#3) Otherwise, traverse with the new name
+		while True:
+			new_node = unvisited[random.randint(0,len(unvisited)-1)]
+			if new_node.name not in a_node.family:
+				break
+		return a_node.name + ' ' + traversal(new_node)
 	
+
 #Read in names
 
 participants = open('names.txt')
-participants = [tuple(names.strip('\n').split(' ')) for names in participants]
-
+participants = [names.strip('\n').split(' ') for names in participants]
+graph = []
+for names in participants:
+	for name in names:
+		family = names[:]
+		family.remove(name)
+		graph.append(node(name, family))
+	
 
 #Create unvisited list
 
-unvisited = [name for names in participants for name in names]
+unvisited = graph
 
 
+#Traverse and see path:
+
+path = traversal(unvisited[random.randint(0,len(unvisited)-1)])
+path = path.split(' ')
+for index, name in enumerate(path):
+	if index == len(path)-1:
+		print name + ' -> ' + path[0]
+	else:
+		print name + ' -> ' + path[index+1] 
